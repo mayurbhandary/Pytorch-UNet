ed7060c6ab29031ebc75709f1dd229a8ce5fccb2
Mayur Bhandary <mayurbhandary96@gmail.com>
2022-01-03 16:16:06 -0500

    modify migrate and download

--- migrate_data.py
+++ migrate_data.py
@@ -11,7 +11,7 @@ images_cells_dir  = './data/imgs_cells'
 masks_cells_dir  = './data/masks_cells'
 mask_ids = [splitext(file)[0][7:10] for file in listdir(masks_cells_dir) if not file.startswith('.')]
 
-original = True #toggle preprocessing
+original = False #toggle preprocessing
 
 # moves and renames mask and image files
 for file in listdir(images_cells_dir):
@@ -22,7 +22,7 @@ for file in listdir(images_cells_dir):
             shutil.copy(images_cells_dir+'/'+file, dest)
         else: 
             im = Image.open(images_cells_dir+'/'+file)
-            im.save(dest, quality = 100, subsampling=0) #irreversible transformation 
+            im.save(dest) 
 
 for file in listdir(masks_cells_dir):
     id = splitext(file)[0]
@@ -32,10 +32,8 @@ for file in listdir(masks_cells_dir):
             shutil.copy(masks_cells_dir+'/'+file, dest)
         else:
             im = Image.open(masks_cells_dir+'/'+file)
-            x = np.asarray(im)
-            normalized = (x.astype(np.uint16) - x.min()) * 255.0 / (x.max() - x.min())
-            im = Image.fromarray(normalized.astype(np.uint8))
-            im.save(dest, quality = 100, subsampling=0)
+            im = im.convert('L')
+            im.save(dest)
 
 
 
--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,21 +1,14 @@
-echo -n "Kaggle username: "
-read USERNAME
-echo ""
-echo -n "Kaggle API key: "
-read APIKEY
+mkdir data/masks_cells 
+mkdir data/imgs_cells
+echo "directories created"
+cd data 
+if [ ! -d "./Fluo-N2DH-GOWT1" ];
+then
+    echo "begin download"
+    curl -sS https://data.celltrackingchallenge.net/training-datasets/Fluo-N2DH-GOWT1.zip > Fluo-N2DH-GOWT1.zip &&
+    unzip Fluo-N2DH-GOWT1.zip 
+    rm Fluo-N2DH-GOWT1.zip &
+fi
+mv Fluo-N2DH-GOWT1/01/* imgs_cells/ 
+mv Fluo-N2DH-GOWT1/01_GT/SEG/* masks_cells/ 
 
-pip install kaggle --upgrade
-mkdir -p ~/.kaggle
-echo "{\"username\":\"$USERNAME\",\"key\":\"$APIKEY\"}" > ~/.kaggle/kaggle.json
-
-kaggle competitions download -c carvana-image-masking-challenge -f train_hq.zip
-unzip train_hq.zip
-mv train_hq/* data/imgs/
-rm -d train_hq
-rm train_hq.zip
-
-kaggle competitions download -c carvana-image-masking-challenge -f train_masks.zip
-unzip train_masks.zip
-mv train_masks/* data/masks/
-rm -d train_masks
-rm train_masks.zip
